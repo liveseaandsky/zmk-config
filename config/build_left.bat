d1897c5a7f852121a7fc74799c8ef1704b046f47
Adam <adamghsmith@googlemail.com>
2023-10-09 21:37:14 +0100

    Set up better trigger for folder opening when building is done

--- config/build_left.bat
+++ config/build_left.bat
@@ -1,4 +1,4 @@
 call C:\Users\adamg\Desktop\ZMK\zephyrproject\.venv\Scripts\activate.bat
 cd "C:\Users\adamg\Desktop\ZMK\zmk\app"  
-start west build -d build/left -b nice_nano_v2 -- -DSHIELD=sofle_left -DZMK_CONFIG="C:/Users/adamg/Desktop/ZMK/zmk-config/config" 
+start /wait west build -d build/left -b nice_nano_v2 -- -DSHIELD=sofle_left -DZMK_CONFIG="C:/Users/adamg/Desktop/ZMK/zmk-config/config" 
 start "" "C:\Users\adamg\Desktop\ZMK\zmk\app\build\left\zephyr\"
