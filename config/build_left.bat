1d0693f769caa2013801eeee5dd85f3164eeae67
Adam <adamghsmith@googlemail.com>
2023-10-09 21:58:39 +0100

    Set up an output file and copy .uf2 files there for easier flashing

--- config/build_left.bat
+++ config/build_left.bat
@@ -1,4 +1,5 @@
 call C:\Users\adamg\Desktop\ZMK\zephyrproject\.venv\Scripts\activate.bat
 cd "C:\Users\adamg\Desktop\ZMK\zmk\app"  
 start /wait west build -d build/left -b nice_nano_v2 -- -DSHIELD=sofle_left -DZMK_CONFIG="C:/Users/adamg/Desktop/ZMK/zmk-config/config" 
-start "" "C:\Users\adamg\Desktop\ZMK\zmk\app\build\left\zephyr\"
+xcopy /v /y "C:\Users\adamg\Desktop\ZMK\zmk\app\build\left\zephyr\zmk.uf2" "C:\Users\adamg\Desktop\ZMK\zmk\app\build\output\left.uf2"
+start "" "C:\Users\adamg\Desktop\ZMK\zmk\app\build\settings_reset\zephyr\zmk.uf2" "C:\Users\adamg\Desktop\ZMK\zmk\app\build\output\"
