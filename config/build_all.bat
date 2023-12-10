80a1255a4881aa816f4966a3f0f577cd4d8c571a
Adam <adamghsmith@googlemail.com>
2023-12-10 22:13:33 +0000

    added .conf for nice view

--- config/build_all.bat
+++ config/build_all.bat
@@ -1,8 +1,8 @@
 call C:\Users\adamg\Desktop\ZMK\zephyrproject\.venv\Scripts\activate.bat
 cd "C:\Users\adamg\Desktop\ZMK\zmk\app"  
 (
-start west build -p -d build/left -b nice_nano_v2 -- -DSHIELD=sofle_left -DZMK_CONFIG="C:/Users/adamg/Desktop/ZMK/zmk-config/config" 
-start west build -p -d build/right -b nice_nano_v2 -- -DSHIELD=sofle_right -DZMK_CONFIG="C:/Users/adamg/Desktop/ZMK/zmk-config/config"
+start west build -p -d build/left -b nice_nano_v2 -- -DSHIELD="sofle_left nice_view_adapter nice_view" -DZMK_CONFIG="C:/Users/adamg/Desktop/ZMK/zmk-config/config" 
+start west build -p -d build/right -b nice_nano_v2 -- -DSHIELD="sofle_right nice_view_adapter nice_view" -DZMK_CONFIG="C:/Users/adamg/Desktop/ZMK/zmk-config/config"
 start west build -p -d build/settings_reset -b nice_nano_v2 -- -DSHIELD=settings_reset -DZMK_CONFIG="C:/Users/adamg/Desktop/ZMK/zmk-config/config"
 ) | pause
 
